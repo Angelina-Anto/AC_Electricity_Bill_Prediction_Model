e810f70ac88328558a435f3fbdf3da9298a9cb24
Angelina Anto <148966352+Angelina-Anto@users.noreply.github.com>
2026-09-18 09:35:44 +0530

    Fix model filename in app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 from sklearn.preprocessing import PolynomialFeatures
 
-model = joblib.load("AC_Electricity_Bill_Prediction.pkl")
+model = joblib.load("A_Electricity_Bill_Prediction.pkl")
 st.title("Electricity Bill prediction Based on AC Units")
 ac_units = st.number_input("Enter AC Units: ", min_value = 0.0, value = 100.0)
 if st.button("Predict"):
