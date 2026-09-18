fdb4702b57bf884294bbcfff1a9b7c7ba5922f40
Angelina Anto <148966352+Angelina-Anto@users.noreply.github.com>
2026-09-18 10:44:26 +0530

    Add fan units input for electricity bill prediction

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from sklearn.preprocessing import PolynomialFeatures
 model = joblib.load("Ac_Electricity_Bill_Prediction.pkl")
 st.title("Electricity Bill prediction Based on AC Units")
 ac_units = st.number_input("Enter AC Units: ", min_value = 0.0, value = 100.0)
+fan_units = st.number_input("Enter Fan Units: ", min_value = 0.0, value = 100.0)
 if st.button("Predict"):
   poly = PolynomialFeatures()
-  ac_units_poly = poly.fit_transform([[ac_units]])
-  prediction = model.predict(ac_units_poly)
+  X_poly = poly.fit_transform([[ac_units, fan_units]])
+  prediction = model.predict(X_poly)
   st.success(f"Predicted Electricity Bill: {prediction[0]:.2f}")
   
